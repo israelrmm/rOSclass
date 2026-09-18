0d515585591278cce5d171e5b57a17e78a49d030
WiFiTiKet <israelrmm@gmail.com>
2026-09-18 09:58:13 -0400

    Remove encrypt and decrypt functions
    Removed encrypt and decrypt functions from ros.class.php.

--- ros.class.php
+++ ros.class.php
@@ -344,26 +344,3 @@ class RouterosAPI
     }
 }
 
-// encrypt decript
-
-function encrypt($string, $key=128) {
-	$result = '';
-	for($i=0, $k= strlen($string); $i<$k; $i++) {
-		$char = substr($string, $i, 1);
-		$keychar = substr($key, ($i % strlen($key))-1, 1);
-		$char = chr(ord($char)+ord($keychar));
-		$result .= $char;
-	}
-	return base64_encode($result);
-}
-function decrypt($string, $key=128) {
-	$result = '';
-	$string = base64_decode($string);
-	for($i=0, $k=strlen($string); $i< $k ; $i++) {
-		$char = substr($string, $i, 1);
-		$keychar = substr($key, ($i % strlen($key))-1, 1);
-		$char = chr(ord($char)-ord($keychar));
-		$result .= $char;
-	}
-	return $result;
-}
